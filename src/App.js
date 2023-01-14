8915fd303beefda066ee90de0a80d4bff9b71353
Shubhamnavale301 <shubhamnavale301@gmail.com>
2023-01-14 19:32:20 +0530

    Functional & Class Component Added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import Hello from './components/hello';
+import Welcome from './components/welcome';
 
 function App() {
   return (
     <div className="App">
+      <Hello />
+      <Welcome />
+      
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello World...!!
         </p>
         <a
           className="App-link"
@@ -15,7 +20,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Click Me
         </a>
       </header>
     </div>
---
+++ src/components/hello.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+function Hello(){
+    return <h1>Hi Shubham this is Function component</h1>
+}
+
+//const Hello = () =><h2>Hi Everyone</h2>     //ES6 Syntax for function Lambda Expression
+export default Hello; 
